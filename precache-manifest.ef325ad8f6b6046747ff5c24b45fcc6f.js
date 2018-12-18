756698808f16c602a66762f3a398da4bfd1c32f4
Mike Bradshaw <mjbradshaw87@gmail.com>
2018-12-17 20:54:00 -0800

    Updates

---
+++ precache-manifest.ef325ad8f6b6046747ff5c24b45fcc6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87aaa27d57881c6117e2",
+    "url": "/triplea-scoreboard/static/css/main.2e6ae41f.chunk.css"
+  },
+  {
+    "revision": "87aaa27d57881c6117e2",
+    "url": "/triplea-scoreboard/static/js/main.87aaa27d.chunk.js"
+  },
+  {
+    "revision": "f75d9b79f41b65865834",
+    "url": "/triplea-scoreboard/static/js/1.f75d9b79.chunk.js"
+  },
+  {
+    "revision": "f2a6c303a1fb40af7231",
+    "url": "/triplea-scoreboard/static/js/runtime~main.f2a6c303.js"
+  },
+  {
+    "revision": "9efa731cfe1f8a2258df913f57d81c81",
+    "url": "/triplea-scoreboard/index.html"
+  }
+];
